f7cd2be04b5f530cdc46d8d012573c550cd7fb28
Muhammad Khysam F <101845963+Khysam@users.noreply.github.com>
2022-05-31 13:15:37 +0700

    Update studikasus7.cpp

--- Praktikum 7/studikasus7.cpp
+++ Praktikum 7/studikasus7.cpp
@@ -35,3 +35,39 @@ cout<<"Input Nama Barang: ";
 cin>>(barang[i].nama);
 cout<<"Input Stock Barang : ";
 cin>>barang[i].stock;
+
+cout<<endl;
+
+}
+}
+void sorting::proses(){
+  for(i=0;i<n;i++){
+    for(b=0;b<n-1;b++){
+      if(barang[b].kode > barang[b+1].kode){
+        temp.kode=barang[b].kode;             
+        barang[b].kode=barang[b+1].kode;
+        barang[b+1].kode=temp.kode; 
+        
+        strcpy(temp.nama,barang[b].nama);            
+        strcpy(barang[b].nama,barang[b+1].nama);
+        strcpy(barang[b+1].nama,temp.nama);
+
+        temp.stock=barang[b].stock;
+        barang[b].stock=barang[b+1].stock;
+        barang[b+1].stock=temp.stock;
+                  
+        
+      }
+    }
+  }
+}
+
+void sorting::output(){
+  cout<<endl;
+  
+        cout<<"\t\t\t"<<"Setelah data diurutkan berdasarkan kode terkecil : "<<endl;
+      cout<<"\t\t\t"<<"-------------------------------------------------------------------"<<endl;
+      cout<<"\t\t\t"<<"|  Kode Barang  |  Nama Barang  |  Stock Barang  |  "<<endl;
+      cout<<"\t\t\t"<<"-------------------------------------------------------------------"<<endl;
+               
+			    for(b=0;b<n;b++){
